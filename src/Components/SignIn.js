c4509232064bf336d0865a73d26a6f73c1b4ea44
Braulio <djbrzhn1@hotmail.com>
2020-11-28 16:51:13 -0600

    intentando agregar a base

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,5 +1,6 @@
 import { Container } from "postcss";
 import React from "react";
+import {db} from "../firebase"
 
 class SingIn extends React.Component {
     constructor(props) {
@@ -18,7 +19,7 @@ class SingIn extends React.Component {
         password2: ''
     }
 
-    handleInputChange({ target }) {
+    handleInputChange( target ) {
         let { name, type } = target
         let valor
         valor = target.value
@@ -27,9 +28,86 @@ class SingIn extends React.Component {
          // })
           console.log(name+" "+valor)
     }
-    addLink = async () => {
-        let { nombre, apellido,identidad,email,Contra} = this.state
+    
+
+    handleNombre = (event) =>{
+        console.log(event.target.value)
+        this.setState({
+            Nombre:event.target.value
+        })
+        
+        
     }
+    handleApellido = (event) =>{
+        console.log(event.target.value)
+        this.setState({
+            Apellido:event.target.value
+        })
+        
+        
+    }
+    handlecontra = (event) =>{
+        console.log(event.target.value)
+        this.setState({
+            password:event.target.value
+        })
+    }
+    handleconfirma = (event) =>{
+        console.log(event.target.value)
+        this.setState({
+            password2:event.target.value
+        })
+    }
+    handleId = (event) =>{
+        console.log(event.target.value)
+        this.setState({
+            identidad:event.target.value
+        })
+    }
+    handleemail = (event) =>{
+        console.log(event.target.value)
+        this.setState({
+            email:event.target.value
+        })
+    }
+    /*function validar_clave(contrasenna)
+		{
+			if(contrasenna.length >= 8)
+			{		
+				var mayuscula = false;
+				var minuscula = false;
+				var numero = false;
+				var caracter_raro = false;
+				
+				for(var i = 0;i<contrasenna.length;i++)
+				{
+					if(contrasenna.charCodeAt(i) >= 65 && contrasenna.charCodeAt(i) <= 90)
+					{
+						mayuscula = true;
+					}
+					else if(contrasenna.charCodeAt(i) >= 97 && contrasenna.charCodeAt(i) <= 122)
+					{
+						minuscula = true;
+					}
+					else if(contrasenna.charCodeAt(i) >= 48 && contrasenna.charCodeAt(i) <= 57)
+					{
+						numero = true;
+					}
+					else
+					{
+						caracter_raro = true;
+					}
+				}
+				if(mayuscula == true && minuscula == true && caracter_raro == true && numero == true)
+				{
+					return true;
+				}
+			}
+			return false;
+		}*/ 
+
+
+
 //espacios en blanco
 /*var espacios = false;
 var cont = 0;
@@ -60,43 +138,52 @@ if (espacios) {
             return true; 
         }
     }
-     handleSubmit=()=>{
-        console.log("hola")
-        console.log()
-        
 
+    
 
+     addUser = () => {
+         console.log("acabas de entrar a esta opcion")
+         db.collection("Usuarios").doc(this.state.Identidad).set({
+            Identidad:this.state.Identidad,
+            Nombre:this.state.Nombre,
+            Apellido:this.state.apellido,
+            Contrasena:this.state.password,
+            Email:this.state.email
+        }).then(()=>{
+            console.log("Maje, felicidades porque agregaaste a la base. ")
+        });
+        console.log("No agrego al parecer :(")
      }
     
     render() {
         return (
             <body background="https://get.wallhere.com/photo/sea-bay-beach-Tourism-hotel-swimming-pool-resort-lagoon-pier-Caribbean-vacation-estate-leisure-ocean-186672.jpg" >
-                <form onSubmit={this.handleSubmit}>
+                <form>
                     <div class="grid min-h-screen place-items-center" >
                         <div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12">
                             <div class="mt-6">
                                 <div class="flex justify-between gap-3">
                                     <span class="w-1/2">
                                         <label for="Nombre" class="block text-xs font-semibold text-gray-600 uppercase">Nombre</label>
-                                        <input id="Nombre" type="text" onChange={this.handleInputChange} name="Nombre" placeholder="Juan" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                                        <input id="Nombre" type="text" onChange={this.handleNombre} name="Nombre" placeholder="Juan" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required/>
                                     </span>
                                     <span class="w-1/2">
                                         <label for="Apellido" class="block text-xs font-semibold text-gray-600 uppercase">Apellido</label>
-                                        <input id="Apellido" type="text" onChange={this.handleInputChange} name="Apellido" placeholder="Perez"  class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                                        <input id="Apellido" type="text" onChange={this.handleApellido} name="Apellido" placeholder="Perez"  class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                     </span>
 
                                 </div>
 
                                 <label for="Identidad" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Identidad</label>
-                                <input id="Identidad" type="text" onChange={this.handleInputChange} name="Identidad" placeholder="0000-0000-0000" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                                <input id="Identidad" type="text" onChange={this.handleId} name="Identidad" placeholder="0000-0000-0000" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
 
                                 <label for="email" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">E-mail</label>
-                                <input id="email" type="email" name="email" onChange={this.handleInputChange} placeholder="john.doe@company.com"  class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                                <input id="email" type="email" name="email" onChange={this.handleemail} placeholder="john.doe@company.com"  class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                 <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Contraseña</label>
-                                <input id="password" type="password" name="password" onChange={this.handleInputChange} placeholder="********" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                                <input id="password" type="password" name="password" onChange={this.handlecontra} placeholder="********" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                 <label for="password2" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Confirma Contraseña</label>
-                                <input id="password2" type="password" onChange={this.handleInputChange} name="password2" placeholder="********"  class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
-                                <button type="submit" onClick={this.handleSubmit} class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+                                <input id="password2" type="password" onChange={this.handleconfirma} name="password2" placeholder="********"  class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                                <button type="submit" onClick={event=> this.addUser} class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
                                     Registrate
                             </button>
                                 <p class="flex justify-between inline-block mt-4 text-xs text-gray-500 cursor-pointer hover:text-black"> ¿Ya estás registrado?</p>
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,20 @@
-import firebase from 'firebase'
+import firebase from "firebase"
+import "firebase/firestore"
+import "firebase/analytics"
+import "firebase/storage"
+import "firebase/auth"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCB_oIEsw30atAUBmY5RLelNnLAW6iDO5M",
-    authDomain: "hotel-e609b.firebaseapp.com",
-    databaseURL: "https://hotel-e609b.firebaseio.com",
-    projectId: "hotel-e609b",
-    storageBucket: "hotel-e609b.appspot.com",
-    messagingSenderId: "986144870862",
-    appId: "1:986144870862:web:8963574a9619bb50481b9f"
-  };
-  // Initialize Firebase
-  const fb = firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCB_oIEsw30atAUBmY5RLelNnLAW6iDO5M",
+  authDomain: "hotel-e609b.firebaseapp.com",
+  databaseURL: "https://hotel-e609b.firebaseio.com",
+  projectId: "hotel-e609b",
+  storageBucket: "hotel-e609b.appspot.com",
+  messagingSenderId: "986144870862",
+  appId: "1:986144870862:web:8963574a9619bb50481b9f"
+};
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig)
 
-  export default fb;
+export default fb;
+export const db = fb.firestore();
