6e7a5648605371498f472d62fefbe88cdf68cc50
Braulio <djbrzhn1@hotmail.com>
2020-11-30 15:53:40 -0600

    listar

---
+++ src/Components/Listar.js
@@ -0,0 +1,15 @@
+class Listar extends React.Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div>
+                <h1>
+                    hola
+                </h1>
+            </div>
+        );
+    }
+}
+export default Listar;
