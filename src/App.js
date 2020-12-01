345ee0b6f9303e28a0fd7086467539a9570c6669
Carlos Nuila <cnuila14@icloud.com>
2020-12-01 10:39:29 -0600

    Arreglando Conflicto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-import React from 'react'
-import RecuperarContra from "./Components/RecuperarContra"
-
-function App() {
-  return (
-    <div className="App">
-      <RecuperarContra />
-    </div>
-=======
 import SingIn from "./Components/SignIn"
 
 function App() {
   return (
     <SingIn/>
->>>>>>> remotes/origin/SingIn
   );
 }
 
