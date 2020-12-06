a4d963b4727c6a72b926b2ac2f0678e9c1c8b9ef
Braulio <djbrzhn1@hotmail.com>
2020-12-05 20:07:25 -0600

    mascaras

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import LogIn from "./Components/LogIn"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AuthProvider } from './Components/Rutas Privadas/Auth';
 import PrivateRoute from "./Components/Rutas Privadas/RutaPrivada"
-
+import SingIn from "./Components/SignIn"
 function App() {
   return (
-    <LogIn/>
+    <SingIn/>
   );
 }
 
