469b298503d331bca1d4e37b6b49ac8790f3b6a6
Carlos Nuila <cnuila14@icloud.com>
2020-12-06 01:45:46 -0600

    Terminando Visual Clientes Admin

--- src/Components/Lista.js
+++ src/Components/Lista.js
@@ -2,60 +2,62 @@ import React from 'react'
 
 export default function Lista(props) {
     return (
-        <div className="grid max-h-screen min-h-screen bg-blue-400">
-            <div className="grid grid-cols-3 m-4 rounded-sm bg-gray-300">
-                <div className="flex flex-col bg-yellow-600 divide-y divide-green-800 rounded-l-sm">
-                    <div className="mx-1 mt-1 p-3 text-center font-semibold text-lg">
+        <div className="max-h-screen">
+            <div className="grid grid-cols-3 max-h-screen bg-gray-100">
+
+                <div className="col-span-1 flex flex-col max-h-screen min-h-screen rounded-l-sm p-2 overflow-y-auto divide-y divide-gray-500 divide-opacity-50">
+                    <div className="bg-gray-300 sticky top-0 opacity-95 rounded-t-md mx-1 mt-1 p-3 text-center font-bold text-lg">
                         Clientes
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
-                    <div className="mx-1 p-4 text-xs">
+                    <div className="mx-1 p-4 text-xs font-semibold">
                         nombre
                     </div>
                 </div>
-                <div className="flex col-span-2 bg-green-600 rounded-r-sm justify-center">
-                    <div className="bg-pink-600 h-full w-10/12 px-20 py-14">
-                        <h1 className="font-bold text-center text-2xl mb-10 text-black m-3"> Nombre y Apellido </h1>                        
-                        <div className="bg-white h-20 my-3 py-4 px-6 rounded-md">
-                            <h2 className="text-gray-500 font-semibold">Email</h2>
+
+                <div className="flex col-span-2 max-h-screen min-h-screen rounded-r-sm justify-center">
+                    <div className="h-full w-10/12 px-20 py-14">
+                        <h1 className="font-bold text-center text-2xl mb-10 text-black m-3"> Nombre y Apellido </h1>
+                        <div className="bg-gray-300 h-20 my-3 py-4 px-6 rounded-md">
+                            <h2 className="text-blue-500 font-semibold">Email</h2>
                             <h2 className="text-black">john.va@va.org</h2>
                         </div>
-                        <div className="bg-white h-20 my-3 py-4 px-6 rounded-md">
-                            <h2 className="text-gray-500 font-semibold">Número de Identidad</h2>
-                            <h2 className="text-black">0801200008813</h2>
-                        </div>                                             
+                        <div className="bg-gray-300 h-20 my-3 py-4 px-6 rounded-md">
+                            <h2 className="text-blue-500 font-semibold">Número de Identidad</h2>
+                            <h2 className="text-black">0801-2000-08813</h2>
+                        </div>
                     </div>
                 </div>
             </div>
