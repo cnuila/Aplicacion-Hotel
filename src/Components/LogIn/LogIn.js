46d632336619dfc5d0d2b99cc530d2d0b32400c6
Carlos Nuila <cnuila14@icloud.com>
2020-12-14 01:18:30 -0600

    Corrigiendo errores

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -7,14 +7,23 @@ import { Link } from 'react-router-dom';
 
 export default function LogIn({ history }) {
 
-    const authGoogle = () => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider).then(result => {
-            console.log("Es primera vez? = ", result.additionalUserInfo.isNewUser)
-            console.log("Info perfil = ", result.additionalUserInfo.profile)
-        }).catch(err => {
-            console.log(err)
-        })
+    //funcion que autentica por google
+    const authGoogle = async () => {
+        var user = firebase.auth().currentUser;
+        if (user) {
+            alert("Debes cerrar sesión primero")
+        } else {
+            const provider = new firebase.auth.GoogleAuthProvider();
+            await auth.signInWithPopup(provider).then(result => {
+                if (result.additionalUserInfo.isNewUser) {
+                    history.push("/signup")
+                } else {
+                    history.push("/");
+                }
+            }).catch(err => {
+                console.log(err)
+            })
+        }
     }
 
     const handleEmailLogin = useCallback(
@@ -32,7 +41,7 @@ export default function LogIn({ history }) {
 
     );
 
-    const currentUser  = useContext(AuthContext);
+    const currentUser = useContext(AuthContext);
 
     if (currentUser) {
         return <Redirect to="/" />;
@@ -80,13 +89,13 @@ export default function LogIn({ history }) {
                             </div>
 
                             <div className="grid grid-cols-2 mt-4 place-items-center">
-                                <div className="flex col-span-2 lg:col-span-1 bg-black hover:bg-gray-900 m-3 h-12 w-56 items-center shadow-lg rounded cursor-pointer">
-                                    <button type="submit"  className="mx-1 text-center w-full text-sm font-semibold text-white text-opacity-90">
+                                <div className="flex col-span-2 bg-black hover:bg-gray-900 m-3 h-12 w-full items-center shadow-lg rounded cursor-pointer">
+                                    <button type="submit" className="mx-1 text-center w-full text-sm font-semibold text-white text-opacity-90">
                                         Iniciar Sesión
                                     </button>
                                 </div>
 
-                                <div className="flex col-span-2 lg:col-span-1 bg-white hover:bg-gray-100 m-3 h-12 w-56 shadow-lg rounded cursor-pointer" onClick={authGoogle}>
+                                <div className="flex col-span-2 lg:col-span-1 bg-white hover:bg-gray-100 m-3 h-12 w-64 shadow-lg rounded cursor-pointer" onClick={authGoogle}>
                                     <svg className="fill-current mx-4 h-6 w-6 self-center" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                         <path d="M113.47 309.408L95.648 375.94l-65.139 1.378C11.042 341.211 0 299.9 0 256c0-42.451 10.324-82.483 28.624-117.732h.014L86.63 148.9l25.404 57.644c-5.317 15.501-8.215 32.141-8.215 49.456.002 18.792 3.406 36.797 9.651 53.408z" fill="#fbbb00" />
                                         <path d="M507.527 208.176C510.467 223.662 512 239.655 512 256c0 18.328-1.927 36.206-5.598 53.451-12.462 58.683-45.025 109.925-90.134 146.187l-.014-.014-73.044-3.727-10.338-64.535c29.932-17.554 53.324-45.025 65.646-77.911h-136.89V208.176h245.899z" fill="#518ef8" />
@@ -94,9 +103,20 @@ export default function LogIn({ history }) {
                                         <path d="M419.404 58.936l-82.933 67.896C313.136 112.246 285.552 103.82 256 103.82c-66.729 0-123.429 42.957-143.965 102.724l-83.397-68.276h-.014C71.23 56.123 157.06 0 256 0c62.115 0 119.068 22.126 163.404 58.936z" fill="#f14336" />
                                     </svg>
                                     <div className="self-center mx-1 text-center text-sm font-semibold text-gray-500 text-opacity-90">
-                                        Ingresa con Google
+                                        Continua con Google
                                     </div>
                                 </div>
+                                <Link to="/signup">
+                                    <div className="flex col-span-2 lg:col-span-1 bg-black hover:bg-gray-900 m-3 h-12 w-64 shadow-lg rounded cursor-pointer">
+                                        <svg className="fill-current mx-4 h-6 w-6 text-white self-center" viewBox="0 0 511.996 511.996" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M230.9 253.371c13.369 8.913 36.827 8.914 50.199-.001l.007-.004 227.865-151.911c-7.474-21.616-28.018-37.188-52.142-37.188H55.166c-24.125 0-44.668 15.572-52.143 37.188l227.87 151.912.007.004z" />
+                                            <path d="M297.746 278.328l-.007.005c-11.702 7.801-26.724 11.702-41.741 11.702-15.02 0-30.036-3.9-41.739-11.703l-.005-.003L0 135.494v257.072c0 30.417 24.747 55.163 55.166 55.163h401.666c30.418 0 55.164-24.746 55.164-55.163V135.494z" />
+                                        </svg>
+                                        <div className="self-center mx-1 text-center text-sm font-semibold text-white text-opacity-90">
+                                            Regístrate por Correo
+                                        </div>
+                                    </div>
+                                </Link>
                             </div>
                         </form>
                     </div>
