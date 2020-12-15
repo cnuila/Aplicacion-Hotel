fccf50ac76a87b5e37af04a50df030a6ba55f303
Braulio <djbrzhn1@hotmail.com>
2020-12-14 20:11:50 -0600

    reseña

--- src/Components/LogIn/SignIn.js
+++ src/Components/LogIn/SignIn.js
@@ -9,7 +9,6 @@ class SingIn extends React.Component {
         super(props)
         this.state = {
             ...this.estadoInicial,
-
         }
     }
     estadoInicial = {
@@ -20,28 +19,15 @@ class SingIn extends React.Component {
         password: '',
         password2: '',
         Telefono: '',
-        
-    }
-
-    handleInputChange(target) {
-        let { name, type } = target
-        let valor
-        valor = target.value
-        //  this.estadoInicial({
-        //    [name]: valor,
-        // })
-        console.log(name + " " + valor)
     }
     handleNombre = (event) => {
-        console.log(event.target.value)
         var Nom = event.target.value
-        Nom =Nom.replaceAll("_", "")
+        Nom = Nom.replaceAll("_", "")
         this.setState({
-            Nombre: event.target.value,
+            Nombre: Nom,
         })
     }
-    handleApellido = (event) => {
-        console.log(event.target.value)
+    handleApellido = (event) => {   
         var Ape = event.target.value
         Ape = Ape.replaceAll("_", "")
         Ape = Ape.replaceAll(" ", "")
@@ -50,19 +36,16 @@ class SingIn extends React.Component {
         })
     }
     handlecontra = (event) => {
-        console.log(event.target.value)
         this.setState({
             password: event.target.value
         })
     }
     handleconfirma = (event) => {
-        console.log(event.target.value)
         this.setState({
             password2: event.target.value
         })
     }
     handleId = (event) => {
-        console.log(event.target.value)
         var ID = event.target.value;
         ID = ID.replaceAll("_", "")
         ID = ID.replaceAll("-", "")
@@ -71,163 +54,72 @@ class SingIn extends React.Component {
         })
     }
     handleemail = (event) => {
-        console.log(event.target.value)
-
         this.setState({
             email: event.target.value
         })
     }
     handleTele = (event) => {
-        console.log(event.target.value)
         var tele = event.target.value;
         tele = tele.replaceAll("_", "")
         this.setState({
             Telefono: tele
         })
     }
-    /*function validar_clave(contrasenna)
-        {
-            if(contrasenna.length >= 8)
-            {		
-                var mayuscula = false;
-                var minuscula = false;
-                var numero = false;
-                var caracter_raro = false;
-            	
-                for(var i = 0;i<contrasenna.length;i++)
-                {
-                    if(contrasenna.charCodeAt(i) >= 65 && contrasenna.charCodeAt(i) <= 90)
-                    {
-                        mayuscula = true;
-                    }
-                    else if(contrasenna.charCodeAt(i) >= 97 && contrasenna.charCodeAt(i) <= 122)
-                    {
-                        minuscula = true;
-                    }
-                    else if(contrasenna.charCodeAt(i) >= 48 && contrasenna.charCodeAt(i) <= 57)
-                    {
-                        numero = true;
-                    }
-                    else
-                    {
-                        caracter_raro = true;
-                    }
-                }
-                if(mayuscula == true && minuscula == true && caracter_raro == true && numero == true)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }*/
-
-
-
-    //espacios en blanco
-    /*var espacios = false;
-    var cont = 0;
-    
-    while (!espacios && (cont < p1.length)) {
-      if (p1.charAt(cont) == " ")
-        espacios = true;
-      cont++;
-    }
-       
-    if (espacios) {
-      alert ("La contraseña no puede contener espacios en blanco");
-      return false;
-    } 
-     */
-    validaespacio(p1, p2) {
-        if (p1.length == 0 || p2.length == 0) {
-            alert("Los campos de la password no pueden quedar vacios");
-            return false;
-        }
-    }
-    contrasigaules(p1, p2) {
-        if (p1 != p2) {
-            alert("Las passwords deben de coincidir");
-            return false;
-        } else {
-            alert("Todo esta correcto");
-            return true;
-        }
-    }
-
-
-
     handleSubmit = (event) => {
-
         event.preventDefault()
-
-        db.collection("Usuarios").doc(this.state.Identidad).set({
-            Identidad: this.state.Identidad,
-            Nombre: this.state.Nombre,
-            Apellido: this.state.Apellido,
-            Contrasena: this.state.password,
-            Email: this.state.email,
-            Telefono: this.state.Telefono,
-
-        }).then(() => {
-            console.log("Agregado a la base de datos ")
-        });
-
-        let email = this.state.email
-        let password = this.state.password
-
-        auth.createUserWithEmailAndPassword(email, password)
-            .then((user) => {
-                console.log("Agrego a la base auth")
-            })
-            .catch((error) => {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.log(errorMessage)
+        // perform all neccassary validations
+        if (this.state.password !== this.state.password2) {
+            alert("las contraseñas no son iguales ")
+        } else {
+            db.collection("Usuarios").doc(this.state.Identidad).set({
+                Identidad: this.state.Identidad,
+                Nombre: this.state.Nombre,
+                Apellido: this.state.Apellido,
+                Contrasena: this.state.password,
+                Email: this.state.email,
+                Telefono: this.state.Telefono,
+            }).then(() => { 
             });
-
+            let email = this.state.email
+            let password = this.state.password
+            auth.createUserWithEmailAndPassword(email, password)
+                .then((user) => {     
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                });
+        }
     }
-
-
     render() {
         return (
-            //<body background="https://scontent.ftgu1-1.fna.fbcdn.net/v/t1.0-9/104181902_10157399492806724_8294953661876225000_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_eui2=AeEEou_0423Aiq6dtRu13DttUSYYPRRfMA9RJhg9FF8wD08Drbpu5wZgHygu_4qYIHViFY6HimWDUa4lDDOZFYy3&_nc_ohc=fScc_TOxxQ8AX8OiH1a&_nc_ht=scontent.ftgu1-1.fna&oh=2473f66cfae5fc9eeb4380a8ba59983c&oe=5FEA1EC9" >
-
             <body background={imagen} >
-
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={ this.handleSubmit}>
                     <div className="grid min-h-screen place-items-center" >
                         <div className="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12">
                             <div className="mt-6">
                                 <div className="flex justify-between gap-3">
                                     <span className="w-1/2">
                                         <label for="Nombre" className="block text-xs font-semibold text-gray-600 uppercase">Nombre</label>
-                                        <input type="name" id="Nombre" disableUnderline onChange={this.handleNombre} name="Nombre" placeholder="Juan" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                                        <InputMask mask="aaaaaaaaaaaaaaa" type="name" id="Nombre" disableUnderline onChange={this.handleNombre} name="Nombre" placeholder="Juan" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                     </span>
                                     <span className="w-1/2">
                                         <label for="Apellido" className="block text-xs font-semibold text-gray-600 uppercase">Apellido</label>
                                         <InputMask mask="aaaaaaaaaaaaaaa" id="Apellido" type="text" onChange={this.handleApellido} name="Apellido" placeholder="Perez" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                     </span>
-
                                 </div>
                                 <label for="Telefono" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Telefono</label>
                                 <InputMask mask="9999-9999" id="Telefono" type="text" disableUnderline onChange={this.handleTele} name="Identidad" placeholder="00000000" maskPlaceholder="-"//placeholder="0000-0000-0000" 
-                                    autoComplete="cc-number"
-                                    className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required
-                                />
-
+                                    autoComplete="cc-number" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required/>
                                 <label for="Identidad" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Identidad-Pasaporte-Carnet de residente (sin guiones)</label>
                                 <InputMask mask="****************" id="Identidad" disableUnderline type="text" onChange={this.handleId} name="Identidad" placeholder="0000-0000-00000" maskPlaceholder="-"//placeholder="0000-0000-0000" 
-                                    autoComplete="cc-number"
-                                    className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required
-                                />
-
+                                    autoComplete="cc-number" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required/>
                                 <label for="email" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">E-mail</label>
                                 <input id="email" type="email" name="email" minlengt="12" onChange={this.handleemail} placeholder="john.doe@company.com" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                 <label for="password" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Contraseña</label>
                                 <input id="password" type="password" minlengt="8" name="password" onChange={this.handlecontra} placeholder="********" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                 <label for="password" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Confirmar Contraseña</label>
-                                <input id="Cpassword" type="password" minlengt="8" name="Cpassword" onChange={this.handlecontra} placeholder="********" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
-
+                                <input id="Cpassword" type="password" minlengt="8" name="Cpassword" onChange={this.handleconfirma} placeholder="********" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                                 <input value="Registrate" type="submit" className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none" />
                                 <Link to="./login" className="flex justify-between  mt-4 text-xs text-gray-500 cursor-pointer hover:text-black"> ¿Ya estás registrado?</Link>
                             </div>
