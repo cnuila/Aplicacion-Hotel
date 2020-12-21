f9849930d7f645085e85f029cdbb0ca1989c3af4
Braulio <djbrzhn1@hotmail.com>
2020-12-21 02:59:06 -0600

    censura

--- src/Components/Comentario.js
+++ src/Components/Comentario.js
@@ -17,9 +17,9 @@ class Comentario extends React.Component {
         const query = await db.collection("Usuarios").where("Identidad", "==", "0801-1999-19890").get();
         query.forEach(doc => {
             this.setState({
-                Rating: doc.data().Rating,
-                Nombre: doc.data().Nombre,
-                comentario: doc.data().comentario,
+                this.state.Rating: doc.data().Rating,
+                this.state.Nombre: doc.data().Nombre,
+                this.state.comentario: doc.data().comentario,
             })
         })
     }*/
@@ -32,20 +32,14 @@ class Comentario extends React.Component {
                         */ }<div class="w-full  p-4">
                             <div class="justify-items-center ">
 
-                                <h3 class="text-pink-50  mx-7 font-semibold text-lg text-center md:text-left ">Braulio </h3>
+                                <h3 class="text-pink-50  mx-7 font-semibold text-lg text-center md:text-left ">{this.state.Nombre} </h3>
                                 <p class="work-sans font-semibold text-xl text-white"></p>
                                 <div class="mx-5 mb-1">
-                                <ReactStarRating numberOfStar={5} numberOfSelectedStar={3} colorFilledStar="yellow" colorEmptyStar="gray" starSize="25px" spaceBetweenStar="8px" disableOnSelect={true} />
+                                <ReactStarRating numberOfStar={5} numberOfSelectedStar={this.state.Rating} colorFilledStar="yellow" colorEmptyStar="gray" starSize="25px" spaceBetweenStar="8px" disableOnSelect={true} />
                                 </div>
-                                <textarea  name="comentario" value="Un Dia estos majes me pusieron algo que se salio de pedo, la cosa es que, nunca pensaron el seguridad de la gente que estaba ahi" class="bg-gray-700 text-xl leading-normal resize-none w-full h-20 py-2 px-3 font-medium text-pink-50 focus:outline-none " name="body"  ></textarea>
-
-
+                                <textarea  name="comentario" value={this.state.comentario} class="bg-gray-700 text-xl leading-normal resize-none w-full h-20 py-2 px-3 font-medium text-pink-50 focus:outline-none " name="body"  ></textarea>
                             </div>
-
-                            
-
                         </div>
-
                     </div>
                 </div>
             </div>
--- src/Components/Reseña.js
+++ src/Components/Reseña.js
@@ -21,7 +21,33 @@ class reseña extends React.Component {
             comentario: event.target.value
         })
     }
-
+    handleSubmit = (event) => {
+        event.preventDefault()
+        // perform all neccassary validations
+        
+        if (this.state.comentario.includes('puta')||this.state.comentario.includes('maldito')||this.state.comentario.includes('puta')||this.state.comentario.includes('cabron')||this.state.comentario.includes('pija')||this.state.comentario.includes('mierda')
+        
+        ){    
+            console.log(this.state.Rating)
+            alert("Su Comentario incluye palabras ofensivas")
+        }else if(this.state.Rating===""){
+            console.log(this.state.Rating)
+            alert("por favor indicar cuantas estrellas")
+        }else if(this.state.comentario.length<5){
+            alert("su comentario es muy corto")
+        } 
+        else {
+            console.log(this.state.Rating)
+            alert("Su Comentario es valido")
+            // poner la base de datos que corresponde con la subcoleccion
+            /*db.collection("").doc(this.state.Identidad).set({
+                rating: this.state.Rating,
+                comentario: this.state.comentario,
+                
+            }).then(() => { 
+            });
+            */}
+    }
 
     render() {
 
@@ -37,14 +63,13 @@ class reseña extends React.Component {
                                     colorEmptyStar="gray" starSize="20px" spaceBetweenStar="8px"
                                     disableOnSelect={false}
                                     onSelectStar={val => {
-                                        this.estadoInicial.Rating = val
-                                        console.log(this.estadoInicial.Rating)
+                                        this.state.Rating = val
                                     }}/>
                                 <div class="w-full md:w-full px-3 mb-2 mt-2">
                                     <textarea name="comentario" onChange={this.handlecomentario} class="bg-gray-100 rounded border  border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder='Danos tu Opinión' required></textarea>
                                 </div>
                                 <div class="w-full justify-center flex items-start md:w-full px-3">
-                                    <input type='submit' class="bg-gray-300  text-gray-900 font-medium py-1 px-4 border-2 border-gray-700 rounded-lg tracking-wide mr-1 hover:bg-gray-400" value='Sube tu comentario' />
+                                    <input type='submit' onClick={this.handleSubmit} class="bg-gray-300  text-gray-900 font-medium py-1 px-4 border-2 border-gray-700 rounded-lg tracking-wide mr-1 hover:bg-gray-400" value='Sube tu comentario' />
                                 </div>
                             </div>
                         </form>
