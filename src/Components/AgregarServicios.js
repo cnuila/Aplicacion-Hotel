461093113fcd8d0f138c19da7d8295f017459aa1
Stephanie Martínez <stephanie_martinezh@hotmail.com>
2020-12-21 01:35:12 -0600

    Arreglo a algunas validaciones

--- src/Components/AgregarServicios.js
+++ src/Components/AgregarServicios.js
@@ -72,9 +72,12 @@ class AgregarServicios extends Component{
         this.alerta(), ()=>{
            console.log("No agrego a servicios")
        });
+        var n1 = (document.getElementById("nombre").value);
+        var n2 = parseInt(document.getElementById("precio").value);
+        n1.value = "";
+        n2.value = 0;
     }
-
-
+    
     handleDelete = (value)=>{
         let elemento=this.state.Detalles
         let indice=elemento.indexOf(value)
@@ -92,13 +95,13 @@ class AgregarServicios extends Component{
                 <div className="grid min-h-screen place-items-center">
                     <div className="w-3/4 p-12 bg-white">
                         <h1 className="text-xl font-semibold text-center">Ingrese información sobre el servicio</h1>
-                        <form className="mt-6">                       
+                        <form  onSubmit={this.handleAgregarServicios} className="mt-6">                       
                             <label className="block mt-2 text-sm font-semibold text-gray-600 uppercase">Nombre del servicio</label>
-                            <input type="text" onChange={this.handleNombre} name="nombre" placeholder="Salón de eventos" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                            <input type="text" id="nombre" onChange={this.handleNombre} name="nombre" placeholder="Salón de eventos" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                             <label className="block mt-2 text-sm font-semibold text-gray-600 uppercase">Precio del servicio</label>
-                            <input type="number" onChange={this.handlePrecio} name="Precio" placeholder="Lps.1500" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                            <input type="number" id="precio" onChange={this.handlePrecio} name="Precio" placeholder="Lps.1500" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                             <label className="block mt-8 text-sm font-semibold text-gray-600 uppercase">Detalles</label>
-                            <input type="text" value={this.state.elemento} onChange={this.handleDetalles} name="Detalles" placeholder="Incluye decoraciones" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"/>
+                            <input type="text" id="detalles" value={this.state.elemento} onChange={this.handleDetalles} name="Detalles" placeholder="Incluye decoraciones" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"/>
                             <div className="w-full">
                               <button onClick={this.handleStateDetalles} className="text-white font-bold p-4 rounded bg-blue-900 hover:bg-indigo-700 px-3 py-2 mt-4">Agregar detalle</button>
                             </div>
@@ -108,7 +111,9 @@ class AgregarServicios extends Component{
                                      <span onClick={()=>this.handleDelete(elemento)} className="cursor-pointer w-2/12 p-3 inline-block bg-red-500 text-center">X</span>
                                 </div>
                             ))}
-                            <input type="submit" value="Agregar Servicio" onClick={this.handleAgregarServicios} className="w-full py-3 mt-10 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none" />
+                            <button type="submit" className="w-full py-3 mt-10 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none" >
+                                Agregar Servicio
+                            </button>   
                                 
                         </form>
                     </div>
