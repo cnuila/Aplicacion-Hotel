1589a5ee41b3ca577133a7bc915492d5cf297f56
Oswaldo <oswavare14@hotmail.com>
2021-03-26 17:12:30 -0600

    mandando correo como admin

--- src/Components/InformacionHabitacion.js
+++ src/Components/InformacionHabitacion.js
@@ -25,6 +25,7 @@ export default function InfoHabitacion({ location, history }) {
   });
   const [administrador, setAdministrador] = useState(false)
   const [nombreCliente, setNombreCliente] = useState("")
+  const [mailCliente, setMailCliente] = useState("")
 
   useEffect(() => {
     getReservas()
@@ -140,7 +141,95 @@ export default function InfoHabitacion({ location, history }) {
   const reservar = async () => {
     const user = auth.currentUser;
 
-    if (administrador && nombreCliente != "") {
+    if (administrador) {
+      if (nombreCliente != "" && mailCliente != "") {
+        if (user) {
+          if (selectedDayRange.to === null && selectedDayRange.from === null) {
+            swal({
+              text: "Debes seleccionar una fecha primero",
+              icon: "warning",
+              button: "Aceptar"
+            });
+          } else {
+            if (selectedDayRange.to === null && selectedDayRange.from !== null) {
+              swal({
+                text: "Debes seleccionar una fecha final o darle click de nuevo al día si solo quieres reservar un día",
+                icon: "warning",
+                button: "Aceptar"
+              });
+            } else {
+              let idCliente = ""
+              await db.collection("Usuarios").where("Email", "==", user.email).get().then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                  idCliente = doc.id;
+                })
+              })
+              if (idCliente === "") {
+                swal({
+                  text: "No tenemos registrado tu ID, elimina tu cuenta, y asegúrate de terminar el proceso de registro",
+                  icon: "warning",
+                  button: "Aceptar"
+                });
+              } else {
+                const { to, from } = selectedDayRange
+                let emailCliente = user.email
+                let fechaFinal = new Date(to.year, to.month - 1, to.day)
+                let fechaInicial = new Date(from.year, from.month - 1, from.day)
+                let moment1 = moment(fechaInicial)
+                let moment2 = moment(fechaFinal)
+                let diferenciaDias = moment2.diff(moment1, 'days') + 1
+                let precioPagar = precio * diferenciaDias
+                db.collection("Reservas").add({
+                  administrador: administrador,
+                  nombreCliente: nombreCliente,
+                  idHabitacion: id,
+                  idCliente,
+                  emailCliente,
+                  fechaFinal,
+                  fechaInicial,
+                  precioPagar,
+                  pagada: false,
+                }).then(() => {
+
+                  swal({
+                    text: "Reservaste con éxito, revisa tu correo!",
+                    icon: "success",
+                    button: "Aceptar"
+                  }).then(() => {
+                    history.push("/misReservas");
+                  });
+
+                  var templateParams = {
+                    name: nombreCliente,
+                    subject: 'Tu habitacion se reservo con exito',
+                    email: mailCliente
+                  };
+
+                  emailjs.send('service_kq0urtv', 'template_si8lrwe', templateParams, 'user_IlfmLUQnITF5aqsX4gKMh')
+                    .then(function (response) {
+                      console.log('SUCCESS!', response.status, response.text);
+                    }, function (error) {
+                      console.log('FAILED...', error);
+                    });
+                })
+              }
+            }
+          }
+        } else {
+          swal({
+            text: "Debes iniciar sesión para hacer una reseña",
+            icon: "warning",
+            button: "Aceptar"
+          });
+        }
+      } else {
+        swal({
+          text: "Indica el nombre o correo del cliente",
+          icon: "warning",
+          button: "Aceptar"
+        });
+      }
+    } else {
       if (user) {
         if (selectedDayRange.to === null && selectedDayRange.from === null) {
           swal({
@@ -189,8 +278,16 @@ export default function InfoHabitacion({ location, history }) {
                 pagada: false,
               }).then(() => {
 
+                swal({
+                  text: "Reservaste con éxito, revisa tu correo!",
+                  icon: "success",
+                  button: "Aceptar"
+                }).then(() => {
+                  history.push("/misReservas");
+                });
+
                 var templateParams = {
-                  name: nombreCliente,
+                  name: "",
                   subject: 'Tu habitacion se reservo con exito',
                   email: user.email
                 };
@@ -201,14 +298,6 @@ export default function InfoHabitacion({ location, history }) {
                   }, function (error) {
                     console.log('FAILED...', error);
                   });
-
-                swal({
-                  text: "Reservaste con éxito, revisa tu correo!",
-                  icon: "success",
-                  button: "Aceptar"
-                }).then(() => {
-                  history.push("/misReservas");
-                });
               })
             }
           }
@@ -220,12 +309,6 @@ export default function InfoHabitacion({ location, history }) {
           button: "Aceptar"
         });
       }
-    } else {
-      swal({
-        text: "Indica el nombre del cliente",
-        icon: "warning",
-        button: "Aceptar"
-      });
     }
   }
 
@@ -282,6 +365,8 @@ export default function InfoHabitacion({ location, history }) {
                     <div>
                       <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Nombre del Cliente</label>
                       <input onChange={event => setNombreCliente(event.target.value)} type="text" name="nombre" placeholder="Carlos Martinez" className="block w-80 p-3 mt-2 text-black bg-white appearance-none focus:outline-none focus:shadow-inner" required />
+                      <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Correo del Cliente</label>
+                      <input onChange={event => setMailCliente(event.target.value)} type="text" name="nombre" placeholder="ejemplo@gmail.com" className="block w-80 p-3 mt-2 text-black bg-white appearance-none focus:outline-none focus:shadow-inner" required />
                     </div>
                     :
                     <></>
